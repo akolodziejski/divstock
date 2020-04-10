9b9e68b788f249f7e0b0efa9f7d914a27b82b0d1
akolodzi <arkadiusz.kolodziejski@intel.com>
2020-04-10 13:07:59 +0200

    Init commit with SPring Boot structure.

---
+++ src/main/java/com/akolodziejski/divstock/DivstockApplication.java
@@ -0,0 +1,13 @@
+package com.akolodziejski.divstock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DivstockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DivstockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/akolodziejski/divstock/DivstockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akolodziejski.divstock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DivstockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
