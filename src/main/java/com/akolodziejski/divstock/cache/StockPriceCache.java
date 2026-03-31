3771df9edaf6e02ee7df85627fea5abcf172dfd7
akolodziejski <arkadiusz.kolodziejski@gmail.com>
2026-03-31 10:54:47 +0200

    fix: migrate javax.annotation.PostConstruct to jakarta

--- src/main/java/com/akolodziejski/divstock/cache/StockPriceCache.java
+++ src/main/java/com/akolodziejski/divstock/cache/StockPriceCache.java
@@ -7,7 +7,7 @@ import org.mapdb.DBMaker;
 import org.mapdb.Serializer;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.PostConstruct;
+import jakarta.annotation.PostConstruct;
 import java.util.Date;
 import java.util.Map;
 
