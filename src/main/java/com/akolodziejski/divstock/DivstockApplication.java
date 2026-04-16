fcf856aca0381a48c17da199a038816e69f582c7
akolodziejski <arkadiusz.kolodziejski@gmail.com>
2026-04-16 15:22:40 +0200

    feat: wire SoldStocksCalculator and SoldStocksReportWriter into application run

--- src/main/java/com/akolodziejski/divstock/DivstockApplication.java
+++ src/main/java/com/akolodziejski/divstock/DivstockApplication.java
@@ -1,15 +1,19 @@
 package com.akolodziejski.divstock;
 
+import com.akolodziejski.divstock.model.reporter.PLNTransaction;
 import com.akolodziejski.divstock.model.tax.Profit;
 import com.akolodziejski.divstock.service.reporter.ExchangeIncomeReportWriter;
+import com.akolodziejski.divstock.service.reporter.SoldStocksReportWriter;
 import com.akolodziejski.divstock.service.tax.ExchangeIncomeCalculator;
 import com.akolodziejski.divstock.service.tax.PIT38Calculator;
+import com.akolodziejski.divstock.service.tax.SoldStocksCalculator;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
+import java.util.List;
 import java.util.Map;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
@@ -17,10 +21,14 @@ public class DivstockApplication implements CommandLineRunner {
 
     private final PIT38Calculator calculator;
     private final ExchangeIncomeCalculator exchangeIncomeCalculator;
+    private final SoldStocksCalculator soldStocksCalculator;
 
-    public DivstockApplication(PIT38Calculator calculator, ExchangeIncomeCalculator exchangeIncomeCalculator) {
+    public DivstockApplication(PIT38Calculator calculator,
+                                ExchangeIncomeCalculator exchangeIncomeCalculator,
+                                SoldStocksCalculator soldStocksCalculator) {
         this.calculator = calculator;
         this.exchangeIncomeCalculator = exchangeIncomeCalculator;
+        this.soldStocksCalculator = soldStocksCalculator;
     }
 
     public static void main(String[] args) {
@@ -37,5 +45,9 @@ public class DivstockApplication implements CommandLineRunner {
         Map<String, Double> exchangeIncome = exchangeIncomeCalculator.calculate(year);
         new ExchangeIncomeReportWriter().write(exchangeIncome, year);
         System.out.println("Exchange income report written to output/exchange_income_" + year + ".csv");
+
+        List<PLNTransaction> soldStocks = soldStocksCalculator.calculate(year);
+        new SoldStocksReportWriter().write(soldStocks, year);
+        System.out.println("Sold stocks report written to output/sold_stocks_" + year + ".csv");
     }
 }
