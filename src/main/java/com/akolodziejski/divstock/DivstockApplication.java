fc3bf7a54c20fa19b6dc5c904f5a33eed7cf779c
akolodziejski <arkadiusz.kolodziejski@gmail.com>
2026-04-16 10:09:56 +0200

    feat: wire ExchangeIncomeCalculator and ReportWriter into application run

--- src/main/java/com/akolodziejski/divstock/DivstockApplication.java
+++ src/main/java/com/akolodziejski/divstock/DivstockApplication.java
@@ -1,8 +1,8 @@
 package com.akolodziejski.divstock;
 
 import com.akolodziejski.divstock.model.tax.Profit;
-import com.akolodziejski.divstock.service.reporter.DivGrowthReporter;
-import com.akolodziejski.divstock.service.tax.OptionsCalculator;
+import com.akolodziejski.divstock.service.reporter.ExchangeIncomeReportWriter;
+import com.akolodziejski.divstock.service.tax.ExchangeIncomeCalculator;
 import com.akolodziejski.divstock.service.tax.PIT38Calculator;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -10,25 +10,32 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
+import java.util.Map;
 
-@SpringBootApplication(exclude={DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
-public class DivstockApplication  implements CommandLineRunner {
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+public class DivstockApplication implements CommandLineRunner {
 
-	private PIT38Calculator calculator;
+    private final PIT38Calculator calculator;
+    private final ExchangeIncomeCalculator exchangeIncomeCalculator;
 
-	public DivstockApplication(PIT38Calculator calculator) {
-		this.calculator = calculator;
+    public DivstockApplication(PIT38Calculator calculator, ExchangeIncomeCalculator exchangeIncomeCalculator) {
+        this.calculator = calculator;
+        this.exchangeIncomeCalculator = exchangeIncomeCalculator;
+    }
 
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DivstockApplication.class, args);
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(DivstockApplication.class, args);
-	}
+    @Override
+    public void run(String... args) throws Exception {
+        int year = 2025;
 
-	@Override
-	public void run(String... args) throws Exception {
-		Profit profit = calculator.calculate(2023);
-		System.out.println(profit);
-	}
+        Profit profit = calculator.calculate(year);
+        System.out.println(profit);
 
+        Map<String, Double> exchangeIncome = exchangeIncomeCalculator.calculate(year);
+        new ExchangeIncomeReportWriter().write(exchangeIncome, year);
+        System.out.println("Exchange income report written to output/exchange_income_" + year + ".csv");
+    }
 }
