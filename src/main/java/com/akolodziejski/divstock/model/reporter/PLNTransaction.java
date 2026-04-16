37d715970d74f07af2ae3c42ed4a1c712cf3ab30
akolodziejski <arkadiusz.kolodziejski@gmail.com>
2026-04-16 14:59:35 +0200

    feat: add costInCurrency and gainLostInCurrency fields to PLNTransaction

--- src/main/java/com/akolodziejski/divstock/model/reporter/PLNTransaction.java
+++ src/main/java/com/akolodziejski/divstock/model/reporter/PLNTransaction.java
@@ -12,6 +12,8 @@ public class PLNTransaction {
     private float gainLost; // TODO remove
     private float cost;
     private float income;
+    private float costInCurrency;         // cost basis in original transaction currency
+    private float gainLostInCurrency;     // gain/loss in original transaction currency
     private double taxPaid;
     private boolean error;
     private Transaction statement;
